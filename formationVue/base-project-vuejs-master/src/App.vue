08600975bd46096a391d57a705124d04ba5a1f44
dbenamara <dbenamara@excilys.com>
2020-07-09 17:03:05 +0200

    refacto rest api + vuetify

--- formationVue/base-project-vuejs-master/src/App.vue
+++ formationVue/base-project-vuejs-master/src/App.vue
@@ -8,122 +8,109 @@
         elevate-on-scroll
         scroll-target="#scrolling-techniques-7"
       >
-        <v-menu transition="slide-x-transition"
-        bottom
-        right>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  dark
-                  icon
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <v-app-bar-nav-icon></v-app-bar-nav-icon>
-                </v-btn>
-              </template>
-                 <div v-if=isLogged>
-              <v-list>
-               <v-list-items>
-      
-        <v-list-item-title><router-link to="/">Home</router-link></v-list-item-title>
-         
-        
-        
-       <v-list-item-title><router-link to="/computers">Computers</router-link></v-list-item-title>
-      <v-list-item-title><router-link to="/companies">Companies</router-link></v-list-item-title>
-      <v-list-item-title><router-link to="/addComputer">Add Computer</router-link></v-list-item-title>
-      <v-list-item-title><router-link to="/editComputer">Edit Computer</router-link></v-list-item-title>
-    </v-list-items>
-              
-      
-              </v-list>
-              </div>
-            </v-menu>
-        
- 
+        <v-menu transition="slide-x-transition" bottom right>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark icon v-bind="attrs" v-on="on">
+              <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </v-btn>
+          </template>
+          <div v-if="isLogged">
+            <v-list>
+              <v-list-items>
+                <v-list-item-title>
+                  <router-link to="/">Home</router-link>
+                </v-list-item-title>
+
+                <v-list-item-title>
+                  <router-link to="/computers">Computers</router-link>
+                </v-list-item-title>
+                <v-list-item-title>
+                  <router-link to="/companies">Companies</router-link>
+                </v-list-item-title>
+                <v-list-item-title>
+                  <router-link to="/addComputer">Add Computer</router-link>
+                </v-list-item-title>
+                <v-list-item-title>
+                  <router-link to="/editComputer">Edit Computer</router-link>
+                </v-list-item-title>
+              </v-list-items>
+            </v-list>
+          </div>
+        </v-menu>
+
         <v-toolbar-title>Application Database</v-toolbar-title>
- 
-        <v-spacer></v-spacer> 
- 
-       
-       
-        <v-menu transition="slide-x-transition"
-        bottom
-        right>
-        <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  dark
-                  icon
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <v-btn icon>
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
-                </v-btn>
-              </template>
-  
-              <v-list>
-                <!--<v-list-item
+
+        <v-spacer></v-spacer>
+
+        <v-menu transition="slide-x-transition" bottom right>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark icon v-bind="attrs" v-on="on">
+              <v-btn icon>
+                <v-icon>mdi-account</v-icon>
+              </v-btn>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <!--<v-list-item
                   v-for="(item, i) in itemsConnection"
                   :key="i"
                   @click=""
                 >
                   <v-list-item-title><router-link to="/">{{ item.title }}</router-link> </v-list-item-title>
-                </v-list-item>-->
-    <v-list-items>
-      
-      <div v-if=!isLogged><v-list-item-title><router-link to="/login" >Login</router-link></v-list-item-title></div>
-      <div v-else>
-        <v-list-item-title><router-link to="/">Home</router-link></v-list-item-title>
-         <v-list-item-title><router-link to="/login">Logout</router-link></v-list-item-title> 
-        </div>
-        
-       <v-list-item-title><router-link to="/register">Register</router-link></v-list-item-title> 
-    </v-list-items>
-</v-list>
-          </v-menu>
-        
+            </v-list-item>-->
+            <v-list-items>
+              <div v-if="!isLogged">
+                <v-list-item-title>
+                  <router-link to="/login">Login</router-link>
+                </v-list-item-title>
+              </div>
+              <div v-else>
+                <v-list-item-title>
+                  <router-link to="/">Home</router-link>
+                </v-list-item-title>
+                <v-list-item-title>
+                  <router-link to="/login">Logout</router-link>
+                </v-list-item-title>
+              </div>
+
+              <v-list-item-title>
+                <router-link to="/register">Register</router-link>
+              </v-list-item-title>
+            </v-list-items>
+          </v-list>
+        </v-menu>
       </v-app-bar>
-      <v-sheet
-        id="scrolling-techniques-7"
-        class="overflow-y-auto"
-        max-height="55" 
-      >
-        <v-container style="height: 1500px;">
- 
-        </v-container>
+      <v-sheet id="scrolling-techniques-7" class="overflow-y-auto" max-height="55">
+        <v-container style="height: 1500px;"></v-container>
       </v-sheet>
     </v-card>
 
-    
-  
     <router-view />
-    <Header/>
+    <Header />
   </v-app>
 </template>
 
 <script>
 export default {
   data: vm => ({
-        isLogged: false,
-        itemsPages: [
-          { title: 'Computers' },
-          { title: 'Companies' },
-          {title: 'Home'},
-        ],
-        itemsConnection: [
-          { title: 'Register' },
-          { title: 'Login' },
-          { title: 'Logout' },
-        ],
-
+    isLogged: false,
+    itemsPages: [
+      { title: "Computers" },
+      { title: "Companies" },
+      { title: "Home" }
+    ],
+    itemsConnection: [
+      { title: "Register" },
+      { title: "Login" },
+      { title: "Logout" }
+    ]
   }),
   beforeUpdate: function() {
-    if(sessionStorage.getItem('token')){
-      this.isLogged=true;
-    }else{
-      this.isLogged=false;
+    if (sessionStorage.getItem("token")) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
     }
   }
 };
