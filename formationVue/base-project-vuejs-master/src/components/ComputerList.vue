2423d49d8c4ec7a8d5b267dca0e353694581af3e
FGouyonExcilys <fgouyon@excilys.com>
2020-07-09 11:20:27 +0200

    Computer List finie

--- formationVue/base-project-vuejs-master/src/components/ComputerList.vue
+++ formationVue/base-project-vuejs-master/src/components/ComputerList.vue
@@ -4,7 +4,7 @@
       href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css"
       rel="stylesheet"
     />
-    <v-container v-for="item in computerList" :key="item.id">
+    <v-container v-for="item in computerListPaginated" :key="item.id">
       <v-card class="mx-auto" max-width="300">
         <v-list>
           <v-list-item>
@@ -12,18 +12,29 @@
               <v-list-item>
                 <v-list-item-title>{{item.name}}</v-list-item-title>
               </v-list-item>
-              <v-list-item-subtitle
-                v-if="item.introducedDate"
-              >Introduced Date: {{item.introducedDate}}</v-list-item-subtitle>
-              <v-list-item-subtitle v-else>No introduced date</v-list-item-subtitle>
+              <v-list-item-subtitle v-if="item.introducedDate">
+                <u>Introduced Date</u>
+                : {{item.introducedDate}}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle v-else>
+                <u>Introduced Date</u>: Ø
+              </v-list-item-subtitle>
               <br />
-              <v-list-item-subtitle
-                v-if="item.discontinuedDate"
-              >Discontinued Date: {{item.discontinuedDate}}</v-list-item-subtitle>
-              <v-list-item-subtitle v-else>No discontinued date</v-list-item-subtitle>
+              <v-list-item-subtitle v-if="item.discontinuedDate">
+                <u>Discontinued Date</u>
+                : {{item.discontinuedDate}}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle v-else>
+                <u>Discontinued Date</u>: Ø
+              </v-list-item-subtitle>
               <br />
-              <v-list-item-subtitle v-if="item.companyDTO">Company: {{item.companyDTO.name}}</v-list-item-subtitle>
-              <v-list-item-subtitle v-else>No company assigned</v-list-item-subtitle>
+              <v-list-item-subtitle v-if="item.companyDTO">
+                <u>Company</u>
+                : {{item.companyDTO.name}}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle v-else>
+                <u>Company</u>: Ø
+              </v-list-item-subtitle>
               <br />
             </v-list-item-content>
           </v-list-item>
@@ -35,13 +46,16 @@
     <v-row justify="center">
       <v-col cols="6">
         <v-container class="max-width">
-          <v-pagination v-model="page" class="my-4" :length="nbPages"></v-pagination>
+          <v-pagination v-model="pagination.page" class="my-4" :length="pages"
+          @input="reloadComputerList"></v-pagination>
         </v-container>
       </v-col>
     </v-row>
-    <v-row align="right">
-      <div class="my-2">
-        <v-btn small>Normal</v-btn>
+    <v-row justify="center">
+      <div class="my-2 text-center">
+        <v-btn small @click="step10()">10</v-btn>
+        <v-btn small @click="step50()">50</v-btn>
+        <v-btn small @click="step100()">100</v-btn>
       </div>
     </v-row>
   </div>
@@ -56,31 +70,52 @@ import ComputersApi from "../api/computers_api";
 export default {
   data: function() {
     return {
+      computerListPaginated: [],
       computerList: [],
-      page: 1,
-      step: 10,
       nbComputers: 1,
-      nbPages: Math.ceil(nbComputers/step),
+      pagination: {
+        page: 1,
+        step: 10,
+      }
     };
   },
   mounted: function() {
-    ComputersApi.findComputersPaginated(this.page, this.size).then(
+    ComputersApi.findComputersPaginated(1, 10).then(
+      response => (this.computerListPaginated = response.data)
+    );
+    ComputersApi.findAll().then(
       response => (this.computerList = response.data)
     );
-    console.log(this.computerList);
-    ComputersApi.findNumberOfComputers().then(response => this.nbComputers = response.date);
   },
-
+  methods: {
+    step10() {
+      this.pagination.step = 10;
+      this.pagination.page = 1;
+      this.reloadComputerList();
+    },
+    step50() {
+      this.pagination.step = 50;
+      this.pagination.page = 1;
+      this.reloadComputerList();
+    },
+    step100() {
+      this.pagination.step = 100;
+      this.pagination.page = 1;
+      this.reloadComputerList();
+    },
+    reloadComputerList(){
+      ComputersApi.findComputersPaginated(
+        this.pagination.page-1,
+        this.pagination.step
+      ).then(response => (this.computerListPaginated = response.data));
+    }
+  },
   computed: {
-    computedStep10() {
-      this.step = 10;
-    },
-    computedStep50() {
-      this.step = 50;
-    },
-    computedStep100() {
-      this.step = 100;
-    },
+    pages() {
+      return this.pagination.step
+        ? Math.ceil(this.computerList.length/ this.pagination.step -1)
+        : 0;
+    }
   }
 };
 </script>
